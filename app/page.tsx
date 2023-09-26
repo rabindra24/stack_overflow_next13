b688c4501fb7cd1aba82590f269982f7ee5c7c1e
rabin2403 <rabindra2402@gmail.com>
2023-09-26 22:57:03 +0530

    publish

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "@/node_modules/next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">Next.js 13 We are comming</h1>
+    </div>
+  );
+}
