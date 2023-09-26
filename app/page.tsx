7de354d0cf585b1a0061cbbf76bb990c14a84eca
rabin2403 <rabindra2402@gmail.com>
2023-09-26 21:36:45 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "@/node_modules/next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 13 We are comming</h1>
-    </div>
-  );
-}
