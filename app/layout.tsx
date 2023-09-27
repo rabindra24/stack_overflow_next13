42daa9218eca65a448380e1a92d9c1c528cfc049
rabin2403 <rabindra2402@gmail.com>
2023-09-27 11:43:40 +0530

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,26 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import {Inter, Space_Grotesk} from 'next/font/google'
+import React from 'react'
+import './globals.css'
+
+const inter = Inter({
+  subsets : ['latin'],
+  weight : ['100','200','300','400','500','600','700','800','900'],
+  variable : '--font-inter'
+})
  
+const spaceGrotesk = Space_Grotesk({
+  subsets : ['latin'],
+  weight : ['300','400','500','600','700'],
+  variable : '--font-spaceGrotesk'
+})
+
 export const metadata = {
-  title: 'Next.js 13 with Clerk',
+  title: 'DevFlow',
+  description : 'A community-driven platform for asking and answering programming questions Get Help, Share knowledge, and collaborate with developers form around the world. Explore topics in web Development, mobile app development, algorithms, data structure, data structure, and more.',
+  icons  : {
+    icon : '/assets/images/site-logo.png'
+  }
 }
  
 export default function RootLayout({
@@ -10,9 +29,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements : {
+          formButtonPrimary : 'primary-gradient',
+          footerActionLink : 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
